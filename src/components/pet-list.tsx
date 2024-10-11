0c8b363444e9366aeeb8731e624560cf41d51285
AditRobertho <aditjeneri@gmail.com>
2024-10-12 01:04:40 +0700

    finished dashboard page

--- src/components/pet-list.tsx
+++ src/components/pet-list.tsx
@@ -1,9 +1,36 @@
+"use client";
+
+import { usePetContext } from "@/lib/hooks";
+import { cn } from "@/lib/utils";
+import Image from "next/image";
+
 export default function PetList() {
+  const { pets, selectedPetId, handleChangeSelcectedPetId } = usePetContext();
+
   return (
-    <ul>
-      <li className="bg-white border-b border-black/[0.08]">
-        <button>Benjamin</button>
-      </li>
+    <ul className="bg-white border-b border-light">
+      {pets.map((pet) => (
+        <li key={pet.id}>
+          <button
+            onClick={() => handleChangeSelcectedPetId(pet.id)}
+            className={cn(
+              "flex items-center h-[70px] w-full cursor-pointer px-5 text-base gap-3 hover:bg-[#eff1f2] focus:bg-[#eff1f2] transition",
+              {
+                "bg-[#eff1f2]": selectedPetId === pet.id,
+              }
+            )}
+          >
+            <Image
+              src={pet.imageUrl}
+              alt="Pet image"
+              width={45}
+              height={45}
+              className="w-[45px] h-[45px] rounded-full object-cover"
+            />
+            <p className="font-semibold">{pet.name} </p>
+          </button>
+        </li>
+      ))}
     </ul>
   );
 }
---
+++ src/contexts/pet-context-provider.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+import { Pet } from "@/lib/types";
+import { Children, createContext, useState } from "react";
+
+type PetContextProviderProps = {
+  data: Pet[];
+  children: React.ReactNode;
+};
+
+type TPetContext = {
+  pets: Pet[];
+  selectedPetId: string | null;
+  selectedPet: Pet | undefined;
+  handleChangeSelcectedPetId: (id: string) => void;
+  numberOfPets: number;
+};
+
+export const PetContext = createContext<TPetContext | null>(null);
+
+export default function PetContextProvider({
+  data,
+  children,
+}: PetContextProviderProps) {
+  //state
+  const [pets, setPets] = useState(data);
+  const [selectedPetId, setSelectedPetId] = useState<string | null>(null);
+
+  //derived state
+  const selectedPet = pets.find((pet) => pet.id === selectedPetId);
+  const numberOfPets = pets.length;
+
+  //event handler
+  const handleChangeSelcectedPetId = (id: string) => {
+    setSelectedPetId(id);
+  };
+
+  return (
+    <PetContext.Provider
+      value={{
+        pets,
+        selectedPetId,
+        selectedPet,
+        handleChangeSelcectedPetId,
+        numberOfPets,
+      }}
+    >
+      {children}
+    </PetContext.Provider>
+  );
+}
